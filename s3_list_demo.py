00d44ea756b8645b17a290c3c919ff73cd0f2c7e
lovae <luhuiyong35@163.com>
2024-07-09 20:29:21 +0800

    Update s3_list_demo.py

--- s3_list_demo.py
+++ s3_list_demo.py
@@ -1,8 +1,8 @@
 import boto3
 
 # 不推荐：直接在代码中硬编码AWS凭证
-aws_access_key_id = 'AKIAWR4QZNN2D2STGPBN'
-aws_secret_access_key = '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'
+aws_access_key_id = 'AKIAWR4QZNN2I3C5FYOG'
+aws_secret_access_key = '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'
 
 # 使用指定的凭证初始化S3客户端
 s3_client = boto3.client(
